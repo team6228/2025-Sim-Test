8db483f9b73596db9bf7762e9f254c93b77c996a
TheMrz-01 <alanarapoglu@gmail.com>
2025-10-23 19:22:09 +0300

    Working elevator

--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
@@ -5,25 +5,27 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 
 public final class ElevatorConstants {
     public static final int kFollowerSparkMacCanId = 3;
+    public static final int kFollowerSparkMaxPort = 9;
     public static final MotorType kFollowerMotorType = MotorType.kBrushless;
     public static final boolean kFollowerMotorInvert = false;
     
     public static final int kLeaderSparkMaxCanId = 4;
+    public static final int kLeaderSparkMaxPort = 10;
     public static final MotorType kLeaderMotorType = MotorType.kBrushless;
     public static final boolean kLeaderMotorInvert = true;
 
     //Elevator sim stuff
-
     public static final double kMinElevatorHeightMeters = 0.0;
     public static final double kMaxElevatorHeightMeters = 1.25;
 
     public static final DCMotor kGearBox = DCMotor.getNEO(2);
-    public static final double kElevatorGearing = 0.75;
+    public static final double kElevatorGearing = 10.0;
     public static final double kCarriageMass = 4.0;
-    public static final double kElevatorDrumRadius = 1.0;
+    public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0);
     /*[TODO] Elevator Sim may have some more parameters */
 
     /*Encoder */
@@ -51,13 +53,13 @@ public final class ElevatorConstants {
     /*Feedback loop config */
     /*[TODO] dosya icindeki sensor bunun yerine kullanilabilir */
     public static final FeedbackSensor kFeedbackSensor = FeedbackSensor.kPrimaryEncoder;
-    public static final double kP = 1.0;
-    public static final double kI = 0.01;
-    public static final double kD = 0.25;
+    public static final double kP = 5.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
     public static final double kIZone = 0.01;
 
     public static final double kS = 0.0;
-    public static final double kG = 0.0;
-    public static final double kV = 0.0;
+    public static final double kG = 0.25;
+    public static final double kV = 0.25;
     public static final double kA = 0.0;
 }
--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorSubsystem.java
@@ -5,12 +5,16 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
-import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
@@ -20,13 +24,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ElevatorSubsystem extends SubsystemBase{
-    private final SparkMaxConfig leaderConfig = new SparkMaxConfig();
-    private final SparkMaxConfig followerConfig = new SparkMaxConfig();
-
-    private final SparkMax followerSpark = new SparkMax(ElevatorConstants.kFollowerSparkMacCanId,
-        ElevatorConstants.kFollowerMotorType);
-    private final SparkMax leaderSpark = new SparkMax(ElevatorConstants.kLeaderSparkMaxCanId,
-        ElevatorConstants.kLeaderMotorType);
+    private final PWMSparkMax followerSpark = new PWMSparkMax(ElevatorConstants.kFollowerSparkMaxPort);
+    private final PWMSparkMax leaderSpark = new PWMSparkMax(ElevatorConstants.kLeaderSparkMaxPort);
 
     private final Encoder encoder = new Encoder(
         ElevatorConstants.kEncoderChannels[0],
@@ -35,6 +34,19 @@ public class ElevatorSubsystem extends SubsystemBase{
 
     //[TODO] Feedback controllers
     //[TODO] May use pwm sim
+
+    private final ProfiledPIDController controller =
+      new ProfiledPIDController(
+          ElevatorConstants.kP,
+          ElevatorConstants.kI,
+          ElevatorConstants.kD,
+          new TrapezoidProfile.Constraints(1.0, 1.0));
+  ElevatorFeedforward feedforwardController =
+      new ElevatorFeedforward(
+          ElevatorConstants.kS,
+          ElevatorConstants.kG,
+          ElevatorConstants.kV,
+          ElevatorConstants.kA);
 
     //[TODO] Check last parameters
     private final ElevatorSim elevatorSim = new ElevatorSim(
@@ -46,15 +58,16 @@ public class ElevatorSubsystem extends SubsystemBase{
     ElevatorConstants.kMaxElevatorHeightMeters,
     true,
     0,
-    0.01,
-    0.0);
-
-    //Was ist das?????
+    0.001,
+    0);
 
     private final EncoderSim encoderSim = new EncoderSim(encoder);
+    private final PWMSim followerSim = new PWMSim(followerSpark);
+    private final PWMSim leaderSim = new PWMSim(leaderSpark);
 
-    private final Mechanism2d mech2d = new Mechanism2d(20,50);
-    private final MechanismRoot2d mech2droot = mech2d.getRoot("Elevator root", 10, 0);
+    //[TODO look into them values]
+    private final Mechanism2d mech2d = new Mechanism2d(2,2);
+    private final MechanismRoot2d mech2droot = mech2d.getRoot("Elevator root", 1, 0);
     private final MechanismLigament2d elevatorMech2d = mech2droot.append(
         new MechanismLigament2d("Elevator", elevatorSim.getPositionMeters(), 90)
     );
@@ -63,25 +76,22 @@ public class ElevatorSubsystem extends SubsystemBase{
         encoder.reset();
         encoder.setDistancePerPulse(ElevatorConstants.kDistancePerPulse);
 
-        followerSpark.isFollower();
-
-        setSparkMaxConfig();
+        leaderSpark.addFollower(followerSpark);
 
         SmartDashboard.putData("Elevator Sim",mech2d);
     }
 
     @Override
     public void periodic(){
-
+        elevatorMech2d.setLength(encoder.getDistance());
     }
 
     @Override
     public void simulationPeriodic(){
         //[TODO] may use sim motor
         //[TODO] may use something other than getVelocity
-        System.out.println(leaderSparkSim.getAppliedOutput());
-        elevatorSim.setInput(leaderSparkSim.getAppliedOutput() * RobotController.getBatteryVoltage() 
-            + followerSparkSim.getAppliedOutput() * RobotController.getBatteryVoltage());
+        elevatorSim.setInput(leaderSim.getSpeed() * RobotController.getBatteryVoltage() 
+        + followerSim.getSpeed() * RobotController.getBatteryVoltage());
 
         elevatorSim.update(0.020);
 
@@ -89,61 +99,22 @@ public class ElevatorSubsystem extends SubsystemBase{
 
         RoboRioSim.setVInVoltage(
             BatterySim.calculateDefaultBatteryLoadedVoltage(elevatorSim.getCurrentDrawAmps()));
-
-        elevatorMech2d.setLength(encoder.getDistance());
     }
 
-    public Command testElevatorCmd(){
-        return this.startEnd(() -> this.testElevator(5),() -> this.testElevator(0));
+    public Command reachGoalCmd(double goal){
+        return this.run(() -> reachGoal(goal));
     }
 
     public void reachGoal(double goal){
         //[TODO] implement controller
 
-        double pidOutput = 0.0;
-        double feedForward = 0.0;
+        controller.setGoal(goal);
 
-        leaderSpark.setVoltage(pidOutput + feedForward);
-    }
-
-    public void testElevator(double voltage){
-        //[TODO] delete and look into logging
-        leaderSpark.setVoltage(voltage);
-    }
-
-    public void setSparkMaxConfig(){
-        leaderConfig
-            .inverted(ElevatorConstants.kLeaderMotorInvert)
-            .smartCurrentLimit(ElevatorConstants.kSmartCurrent)
-            .idleMode(ElevatorConstants.kIdleMode);
-        leaderConfig.encoder
-        /*[TODO] constantstaki primary encoder yerine bizim encoder i koyma isi */
-            .positionConversionFactor(ElevatorConstants.kPositionConversationFactor)
-            .velocityConversionFactor(ElevatorConstants.kVelocityConversationFactor);
-        leaderConfig.closedLoop
-            //[TODO] feedbackSensor verisini constantstan ek
-            .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-            .pid(ElevatorConstants.kP,ElevatorConstants.kI,ElevatorConstants.kD)
-            .iZone(ElevatorConstants.kIZone);
-
-        followerConfig
-            .follow(ElevatorConstants.kLeaderSparkMaxCanId)
-            .inverted(ElevatorConstants.kFollowerMotorInvert)
-            .smartCurrentLimit(ElevatorConstants.kSmartCurrent)
-            .idleMode(ElevatorConstants.kIdleMode);
-        followerConfig.encoder
-        /*[TODO] constantstaki primary encoder yerine bizim encoder i koyma isi */
-            .positionConversionFactor(ElevatorConstants.kPositionConversationFactor)
-            .velocityConversionFactor(ElevatorConstants.kVelocityConversationFactor);
-        followerConfig.closedLoop
-            //[TODO] feedbackSensor verisini constantstan ek
-            .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-            .pid(ElevatorConstants.kP,ElevatorConstants.kI,ElevatorConstants.kD)
-            .iZone(ElevatorConstants.kIZone);
-
-        //[TODO] look in depth
-        leaderSpark.configure(leaderConfig, null, null);
-        followerSpark.configure(followerConfig, null, null);
+        // With the setpoint value we run PID control like normal
+        double pidOutput = controller.calculate(encoder.getDistance());
+        double feedforwardOutput = feedforwardController.calculate(
+            controller.getSetpoint().velocity);
+        leaderSpark.setVoltage(pidOutput + feedforwardOutput);
     }
 
     /* 
